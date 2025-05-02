aa6bb12ee67ece42b1a96227ba8ac09680df9b2c
Rodrigo Eduardo da Silva <rodrigoeduardo347@gmail.com>
2025-05-01 21:08:23 -0300

    criando banco de dados de forma automática

--- loginService/src/config/db.js
+++ loginService/src/config/db.js
@@ -9,4 +9,26 @@ const pool = new Pool({
     port: config.databasePort
 });
 
+
+async function validateDatabase() {
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS public.usuarios (
+                id SERIAL PRIMARY KEY,
+                nome VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                senha VARCHAR(255) NOT NULL
+            );    
+        `);
+        return true;
+    } catch (error) {
+        console.error('Erro ao conectar ao banco de dados:', error);
+        return false;
+    }
+}
+
+setTimeout(() => {
+    validateDatabase();
+}, 3000);
+
 module.exports = pool;
