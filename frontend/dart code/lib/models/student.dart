7342303e166d67c52e614d10949507b7a5545705
Tarcyo <55460663+Tarcyo@users.noreply.github.com>
2025-05-04 17:08:00 -0300

    Add files via upload

---
+++ frontend/dart code/lib/models/student.dart
@@ -0,0 +1,49 @@
+class Student {
+  int _id;
+  String _name;
+  String _email;
+  String _registration;
+
+  Student({
+    required int id,
+    required String name,
+    required String email,
+    required String registration,
+  })  : _id = id,
+        _name = name,
+        _email = email,
+        _registration = registration;
+
+  // Getters
+  int get id => _id;
+  String get name => _name;
+  String get email => _email;
+  String get registration => _registration;
+
+  // Setters
+  set id(int value) {
+    _id = value;
+  }
+
+  set name(String value) {
+    _name = value;
+  }
+
+  set email(String value) {
+    _email = value;
+  }
+
+  set registration(String value) {
+    _registration = value;
+  }
+
+  @override
+  String toString() {
+    return 'Student{'
+        'id: $_id, '
+        'name: $_name, '
+        'email: $_email, '
+        'registration: $_registration'
+        '}';
+  }
+}
